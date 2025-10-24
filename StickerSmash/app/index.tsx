318fae429ec6890f7cd60576e58bcc19bfb0a5f7
Tatiana Ribeiro <tatianaribeiro790@gmail.com>
2025-10-23 21:22:56 -0300

    essa é a segunda estapa

--- StickerSmash/app/index.tsx
+++ StickerSmash/app/index.tsx
@@ -1,9 +1,13 @@
-import { Text, View,  StyleSheet } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+ import { Link } from 'expo-router'; 
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home screen</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About screen
+      </Link>
     </View>
   );
 }
@@ -18,4 +22,10 @@ const styles = StyleSheet.create({
   text: {
     color: '#fff',
   },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
 });
+
